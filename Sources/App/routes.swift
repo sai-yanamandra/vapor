7e3b6da05a4e143a730a532a5017ea7e7e0f3059
Sai Yanamandra <sai.yanamandra@sumerusolutions.com>
2018-12-21 10:00:57 +0530

    removed unnecessary code

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,11 +12,6 @@ public func routes(_ router: Router) throws {
         return "Hello Vapor"
     }
     
-//    public func get<T>(_ path: PathComponentsRepresentable..., use closure: @escaping (Request) throws -> T) -> Route<Responder>
-//        where T: ResponseEncodable
-//    {
-//        return _on(.GET, at: path.convertToPathComponents(), use: closure)
-//    }
     
     router.get("date") { req -> String in
         let date = Date()
@@ -30,45 +25,7 @@ public func routes(_ router: Router) throws {
         let countVal = try req.parameters.next(Int.self)
         return (CountJSON(count:countVal))
     }
-//    router.get("date") { req in
-//
-//    }
-    
-//    router.get("date") { req in
-//        var date = DateTime.Now
-//        return String(date)
-//    }
-//    public func post<C, T>(_ content: C.Type, at path: PathComponentsRepresentable..., use closure: @escaping (Request, C) throws -> T) -> Route<Responder>
-//        where C: RequestDecodable, T: ResponseEncodable
-//    {
-//        return _on(.POST, at: path.convertToPathComponents(), use: closure)
-//    }
-    
-//    router.post(InfoData.self, at: "info") { req, data -> String in
-//        return "Hello \(data.name)"
-//    }
 
-    router.post(InfoData.self, at: "infojson") { req, data -> InfoResponse in
-        return InfoResponse(request: data)
-    }
-    
-    router.post(UserInfoData.self, at: "user-info") { req, userInfo -> String in
-        //let userInfo = req as UserInfo
-       // var userInfo: UserInfoData
-        //let userInfo = try req.content.decode(UserInfoData.self)
-        //let info = try req.parameters.next(UserInfoData.self)
-        //return "Hello \(userInfo.name), you are \(userInfo.age)"
-        //return "Hello \(userInfo.name), you are \(userInfo.age)"
-        return "Hello"
-        
-    }
-    
-  
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
 }
 
 struct InfoData: Content {
